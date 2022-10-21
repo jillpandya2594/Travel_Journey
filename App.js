069687e2fefdf1a3bb0a6c4bf8dd565b38d860a8
jill pandya <jillpandya2594@gmail.com>
2022-10-21 10:24:31 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react"
-import Header from "Header"
-import Card from "Card"
-import data from "data"
+import Header from "./Header"
+import Card from "./Card"
+import data from "./data"
 
 console.log(data)
  export default function App(){
