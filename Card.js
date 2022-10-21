82764305ec62949337a99c46a0f8bfcb3b1b4caa
jill pandya <jillpandya2594@gmail.com>
2022-10-21 16:25:36 +0530

    Update Card.js

--- Card.js
+++ Card.js
@@ -3,9 +3,9 @@ import React from "react"
 export default function Card(props){
     console.log(props)
     let badgeText
-    if (props.item.travelled === 0) {
+    if (props.travelled === 0) {
         badgeText = "Not Explored Yet"
-    } else if (props.item.travelled === 1) {
+    } else if (props.travelled === 1) {
         badgeText = "Explored!"
     }
     return (
@@ -13,16 +13,16 @@ export default function Card(props){
         <div className="card">
          {badgeText && <div className="card--badge">{badgeText}</div>}
         <div className="imageContainer">
-       <img src={props.item.imageUrl} className="image"/>
+       <img src={props.imageUrl} className="image"/>
        </div>
        <div className="card--info">
        <div ClassName="info--1">
-       <span className="location">{props.item.location}  </span>
-       <span><a href={props.item.googleMapsUrl} className="grey"> View on Google Maps</a></span>
+       <span className="location">{props.location}  </span>
+       <span><a href={props.googleMapsUrl} className="grey"> View on Google Maps</a></span>
       </div>
-       <h1 className="title">{props.item.title}</h1>
-       <span>{props.item.startDate}-{props.item.endDate}</span>
-       <p>{props.item.description}</p>
+       <h1 className="title">{props.title}</h1>
+       <span>{props.item.startDate}-{props.endDate}</span>
+       <p>{props.description}</p>
       
        </div>
        </div>
